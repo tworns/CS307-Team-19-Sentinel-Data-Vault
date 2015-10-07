5306e76e31ad23723e4678c2e506371dec8234ff
Jiho Choi <choi296@purdue.edu>
2015-10-07 12:53:08 -0400

    mainView

--- src/MainView.java
+++ src/MainView.java
@@ -1,6 +1,7 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class MainView {
 
@@ -36,6 +37,18 @@ public class MainView {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(6, 6, 117, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(182, 6, 117, 29);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(6, 41, 117, 29);
+		frame.getContentPane().add(btnNewButton_2);
 	}
-
 }
