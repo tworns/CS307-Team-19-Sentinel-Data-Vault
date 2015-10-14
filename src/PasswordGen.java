cd2e1a9614109a5b01dd870101fc683a35eb36f6
tworns <tworns@purdue.edu>
2015-10-14 13:46:18 -0400

    Added PasswordGen.java
    PasswordGen is the password generator class, does not currently generate
    secure passwords

---
+++ src/PasswordGen.java
@@ -0,0 +1,40 @@
+package sentinelSecurity;
+import java.util.*;
+public class PasswordGen {
+	public int length;
+	public int specials;
+	public int num;
+	public int upper;
+	public PasswordGen(int length, int specials, int num, int upper) { 
+		this.length = length;
+		this.specials = specials;
+		this.num = num;
+		this.upper = upper;
+		
+		genny(length, specials, num, upper);
+		
+	}
+	
+	
+		public String genny(int length, int specials, int num, int upper){
+			Random r = new Random();	
+			r.nextInt();
+			char [] pass = new char[length];
+			for(int i = 0; i < length; i++){ 
+				if(r.nextInt() <= 1 || r.nextInt() >=1) {
+					pass[i] = 'a';
+				}
+				else { 
+					pass[i] = '0';
+				}
+				
+		}
+			//TODO Fix this. Currently returns the hex(?) value of the char [] object. 
+			//Maybe use secure password API 
+		return pass.toString();
+		
+			
+	}
+}
+
+
