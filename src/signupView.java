c865305edd9c27c3a55ca995910a437f43f6532d
Dingfu Sun <stevesunmkii@gmail.com>
2015-10-14 02:40:19 -0400

    change

--- src/signupView.java
+++ src/signupView.java
@@ -40,6 +40,7 @@ public class signupView extends JFrame {
 	 * Create the frame.
 	 */
 	public signupView() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
