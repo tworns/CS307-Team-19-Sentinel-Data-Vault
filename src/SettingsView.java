c8d623e680b5ccbfa7eaf53ef65ffc4b044e80f9
Thomas Worns <tworns@yahoo.com>
2015-10-14 12:38:49 -0400

    Updated SecurityView, SettingsView, and added PasswordGen.java

--- src/SettingsView.java
+++ src/SettingsView.java
@@ -1,10 +1,11 @@
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.event.*;
 
 public class SettingsView {
 
-	private JFrame frame;
+	private JFrame frmSettings;
 
 	/**
 	 * Launch the application.
@@ -14,7 +15,7 @@ public class SettingsView {
 			public void run() {
 				try {
 					SettingsView window = new SettingsView();
-					window.frame.setVisible(true);
+					window.frmSettings.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +34,62 @@ public class SettingsView {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSettings = new JFrame();
+		frmSettings.setTitle("Settings");
+		frmSettings.setBounds(100, 100, 450, 300);
+		frmSettings.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSettings.getContentPane().setLayout(null);
+		
+		JCheckBox chckbxHighSecurityLevel = new JCheckBox("High Security Level ");
+		chckbxHighSecurityLevel.setToolTipText("Toggling this setting will change ALL user encryptions to the strongest possible.");
+		chckbxHighSecurityLevel.setBounds(48, 53, 148, 25);
+		frmSettings.getContentPane().add(chckbxHighSecurityLevel);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerNumberModel(1, 1, 7, 1));
+		spinner.setBounds(48, 87, 30, 22);
+		frmSettings.getContentPane().add(spinner);
+		
+		JLabel lblMaxLockoutTime = new JLabel("Max Lockout Time (Days)");
+		lblMaxLockoutTime.setBounds(90, 90, 159, 16);
+		frmSettings.getContentPane().add(lblMaxLockoutTime);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Weekly", "Monthly", "Every 3 Months", "Every 6 Months", "Yearly"}));
+		comboBox.setBounds(47, 127, 113, 22);
+		frmSettings.getContentPane().add(comboBox);
+		
+		JLabel lblBackupFrequency = new JLabel("Backup Frequency");
+		lblBackupFrequency.setBounds(176, 130, 113, 16);
+		frmSettings.getContentPane().add(lblBackupFrequency);
+		
+		JLabel lblUserSettings = new JLabel("User Settings");
+		lblUserSettings.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblUserSettings.setBounds(49, 13, 118, 25);
+		frmSettings.getContentPane().add(lblUserSettings);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"10 MB", "50 MB", "100 MB", "512 MB", "1 GB", "2 GB", "3 GB", "4 GB", "5 GB"}));
+		comboBox_1.setBounds(48, 162, 113, 22);
+		frmSettings.getContentPane().add(comboBox_1);
+		
+		JLabel lblFileSizeLimit = new JLabel("File Size Limit Warning ");
+		lblFileSizeLimit.setBounds(176, 165, 142, 16);
+		frmSettings.getContentPane().add(lblFileSizeLimit);
+		
+		JButton btnOk = new JButton("Ok");
+		btnOk.setBounds(65, 217, 97, 25);
+		frmSettings.getContentPane().add(btnOk);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent clickCancel) {
+				if(clickCancel.getActionCommand().equalsIgnoreCase("Cancel")) {
+					frmSettings.dispose();
+				}
+			}
+		});
+		btnCancel.setBounds(191, 217, 97, 25);
+		frmSettings.getContentPane().add(btnCancel);
 	}
-
 }
