32f75b4663c720507babb06af1c2c74854bb351d
Jiho Choi <choi296@purdue.edu>
2015-10-15 20:23:44 -0400

    SignupView Security Question Added

--- src/SignupView.java
+++ src/SignupView.java
@@ -19,6 +19,7 @@ public class SignupView extends JFrame {
 	private JTextField usernameField;
 	private JPasswordField passwordField1;
 	private JPasswordField passwordField2;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -50,28 +51,28 @@ public class SignupView extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("E-mail:");
-		lblNewLabel.setBounds(79, 53, 56, 18);
+		lblNewLabel.setBounds(99, 53, 56, 18);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Password:");
-		lblNewLabel_1.setBounds(73, 84, 72, 18);
+		lblNewLabel_1.setBounds(83, 83, 72, 18);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblReenter = new JLabel("Re-enter:");
-		lblReenter.setBounds(73, 115, 72, 18);
+		lblReenter.setBounds(83, 113, 72, 18);
 		contentPane.add(lblReenter);
 		
 		usernameField = new JTextField();
-		usernameField.setBounds(159, 50, 160, 24);
+		usernameField.setBounds(169, 50, 160, 24);
 		contentPane.add(usernameField);
 		usernameField.setColumns(10);
 		
 		passwordField1 = new JPasswordField();
-		passwordField1.setBounds(159, 81, 160, 24);
+		passwordField1.setBounds(169, 80, 160, 24);
 		contentPane.add(passwordField1);
 		
 		passwordField2 = new JPasswordField();
-		passwordField2.setBounds(159, 112, 160, 24);
+		passwordField2.setBounds(169, 110, 160, 24);
 		contentPane.add(passwordField2);
 		
 		JButton btnNewButton = new JButton("Creat account!");
@@ -97,8 +98,24 @@ public class SignupView extends JFrame {
 				}
 			}
 		});
-		btnNewButton.setBounds(158, 172, 145, 27);
+		btnNewButton.setBounds(159, 230, 145, 27);
 		contentPane.add(btnNewButton);
+		
+		JLabel lblSecurityQuestion = new JLabel("Security Question:");
+		lblSecurityQuestion.setBounds(33, 143, 129, 18);
+		contentPane.add(lblSecurityQuestion);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"What is your mother's name?", "What is your first pet's name?", "Where is your favorite city?"}));
+		comboBox.setBounds(169, 140, 253, 27);
+		contentPane.add(comboBox);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(169, 172, 160, 24);
+		contentPane.add(passwordField);
+		
+		JLabel lblAnswer = new JLabel("Answer:");
+		lblAnswer.setBounds(99, 173, 56, 18);
+		contentPane.add(lblAnswer);
 	}
-
 }
