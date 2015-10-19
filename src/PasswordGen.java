4e9ece913e2331cf8e90dac451c96c9f739e6dca
Jiho Choi <choi296@purdue.edu>
2015-10-19 13:03:07 -0400

    API comment added
    API comment added

--- src/PasswordGen.java
+++ src/PasswordGen.java
@@ -1,6 +1,18 @@
 
 import java.util.*;
+
+
+
+/*
+ * APIs
+ * PasswordGenerator API Class
+ * 		http://javadoc.iaik.tugraz.at/iaik_jce/current/iaik/utils/PasswordGenerator.html
+ * */
+
 public class PasswordGen {
+	
+	
+	
 	public int length;
 	public int specials;
 	public int num;
@@ -10,43 +22,43 @@ public class PasswordGen {
 		this.specials = specials;
 		this.num = num;
 		this.upper = upper;
-		
+
 		generator(length, specials, num, upper);
-		
+
 	}
-	
-	
-		public String generator(int length, int specials, int num, int upper){
-			Random r = new Random();	
-			r.nextInt();
-			char [] pass = new char[length];
-			//TODO Fix this. Currently broken, returns string of length+1 for length >= 10. 
-			//Maybe use secure password API 
-			String out = pass.toString();
-			char[] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-			char[] numbers = {'1','2', '3','4', '5', '6', '7', '8','9', '0'};
-			char[] punct = {'!','@','#','$','%'};
-			out = out.substring(3,out.length()); //takes the string representation of the char[]
-			StringBuilder s = new StringBuilder(out);
-			for(int i = out.length()+1; i < length; i++) {  //adds to the string representation of the char[] to meet length. Probably super unsecure.
-			
-				if( i >= length) { 
-					return s.toString();
-				}
-				if(i%3 == 1) {
-					s.append(alpha[(i%26)]);
-				}
-				if (i%3 == 2) { 
-					s.append(punct[i%5]);
-				}
-				else { 
-					s.append(numbers[(i%10)]);
-				}
+
+
+	public String generator(int length, int specials, int num, int upper){
+		Random r = new Random();	
+		r.nextInt();
+		char [] pass = new char[length];
+		//TODO Fix this. Currently broken, returns string of length+1 for length >= 10. 
+		//Maybe use secure password API 
+		String out = pass.toString();
+		char[] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		char[] numbers = {'1','2', '3','4', '5', '6', '7', '8','9', '0'};
+		char[] punct = {'!','@','#','$','%'};
+		out = out.substring(3,out.length()); //takes the string representation of the char[]
+		StringBuilder s = new StringBuilder(out);
+		for(int i = out.length()+1; i < length; i++) {  //adds to the string representation of the char[] to meet length. Probably super unsecure.
+
+			if( i >= length) { 
+				return s.toString();
 			}
-		
+			if(i%3 == 1) {
+				s.append(alpha[(i%26)]);
+			}
+			if (i%3 == 2) { 
+				s.append(punct[i%5]);
+			}
+			else { 
+				s.append(numbers[(i%10)]);
+			}
+		}
+
 		return s.toString();
-		
-			
+
+
 	}
 }
 
