bc77b14eec4b70952a6487e2dffde0dfe79fc4d9
Adam Petty <iamadampetty@gmail.com>
2015-10-26 13:57:16 -0400

    added data_key field to "users" table in database

--- src/controllers/DatabaseManager.java
+++ src/controllers/DatabaseManager.java
@@ -10,21 +10,30 @@ public class DatabaseManager {
 	
 	private static Connection connectToDatabase() {
 		Connection connection = null;
-		// Establish connection and open the database
+		// Establish connection to the existing database
 		try {
 			Class.forName("org.sqlite.JDBC");
 			connection = DriverManager.getConnection("jdbc:sqlite:vault_database");
 			connection.setAutoCommit(false);
 		} catch (Exception e) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.err.println( e.getClass().getName() + ": " + e.getMessage());
 			//e.printStackTrace();
 		}
-		
 		return connection;
 	}
 	
 	public void addUserToDatabase(User newUser) {
 		Connection DBconnection = connectToDatabase();
+		try {
+			Statement stmt = DBconnection.createStatement();
+			// Insert the user account into the "users" table
+		    String sql = "INSERT INTO users (user_email, password_hash, security_question, security_answer, last_login, high_security, account_wipe_set, backup_frequency, max_backup_size) " +
+		          "VALUES (2, 'Becky', 31, 'New York', 30000.00);";
+		    stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	public String retrievePassword(String userEmail) {
