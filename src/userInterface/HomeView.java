ac17b1487faa02bd50685247bc76581f231072fa
Jiho Choi <choi296@purdue.edu>
2015-10-31 15:52:19 -0400

    HomeView updated

--- src/userInterface/HomeView.java
+++ src/userInterface/HomeView.java
@@ -14,6 +14,15 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import java.awt.GridLayout;
 import javax.swing.JTree;
+import net.miginfocom.swing.MigLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JList;
+import java.awt.Color;
 
 public class HomeView {
 
@@ -54,30 +63,55 @@ public class HomeView {
 			
 //		anel_south		
 		JPanel panel_north = new JPanel();
-		panel_north.setBounds(0, 0, 700, 48);			// margins between object 8 
+		panel_north.setBounds(0, 0, 700, 50);			// margins between object 8 
 		panel_north.setOpaque(false); // to set transparent panel
 		frmSentinelDataVault.getContentPane().add(panel_north);
-		panel_north.setLayout(new GridLayout(0, 3, 0, 0));
-	
-		JButton btnSignOut = new JButton("Sign Out");
-		panel_north.add(btnSignOut);
+		
+			JButton btnSignOut = new JButton("Sign Out");
 		
 		JButton btnSetting = new JButton("Setting");
-		panel_north.add(btnSetting);
+		btnSetting.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnAddData = new JButton("Add Data");
 		
 		JButton btnSecurity = new JButton("Security");
-		panel_north.add(btnSecurity);
+		btnSecurity.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		
-		JPanel panel_south = new JPanel();
-		panel_south.setBounds(0, 48, 700, 408);
-		frmSentinelDataVault.getContentPane().add(panel_south);
-		
-
-		
-		JLabel background_main = new JLabel("");
-		background_main.setBounds(0, 0, 700, 456);
-		background_main.setIcon(new ImageIcon("/Users/Jiho/Desktop/사진/6790918-free-background-wallpaper.jpg"));
-		frmSentinelDataVault.getContentPane().add(background_main);
+		JButton btnDeleteData = new JButton("Delete");
+		GroupLayout gl_panel_north = new GroupLayout(panel_north);
+		gl_panel_north.setHorizontalGroup(
+			gl_panel_north.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel_north.createSequentialGroup()
+					.addGap(0, 0, Short.MAX_VALUE)
+					.addComponent(btnSignOut, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnSetting, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnAddData, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnDeleteData, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnSecurity, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+					.addGap(462))
+		);
+		gl_panel_north.setVerticalGroup(
+			gl_panel_north.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_north.createSequentialGroup()
+					.addGroup(gl_panel_north.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnSignOut, GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+						.addComponent(btnAddData, GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+						.addComponent(btnSetting, GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+						.addComponent(btnDeleteData, GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+						.addComponent(btnSecurity, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE))
+					.addGap(0))
+		);
+		panel_north.setLayout(gl_panel_north);
 		
 
 		
