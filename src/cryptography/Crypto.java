b5b2606525a6b1a217cdcf17cecedc43e7ac9fd7
tworns <tworns@purdue.edu>
2015-11-07 17:03:16 -0500

    Crypto decrypt method written
    Again, needs testing. Probably going to have to unscrew a LOT of
    exceptions being thrown and try catch stuff.

--- src/cryptography/Crypto.java
+++ src/cryptography/Crypto.java
@@ -2,12 +2,13 @@ package cryptography;
 
 import dataManagement.*;
 import sun.misc.*;
+
+import java.io.IOException;
 import java.security.*;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 
 import java.util.ArrayList;
-import java.util.Base64.Encoder;
 import java.util.List;
 public class Crypto {
 	
@@ -33,10 +34,10 @@ public class Crypto {
 		return key;
 	}
 	public DataEntry encrypt(User user, DataEntry data) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException { //Return type is temporary
-		Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		 //TODO GET THIS KEY IN A USER
-		List<String> dataList = new ArrayList<String>();
+				
 		try {
+			List<String> dataList = new ArrayList<String>();
+			Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
 			Key key = keyGen(user); // makes a key from the user's data key.
 			c.init(Cipher.ENCRYPT_MODE, key);
 			for(String entry : data.getFieldDataList()){	
@@ -52,7 +53,26 @@ public class Crypto {
 		}
 		return data;
 	}
-	public DataEntry decrypt(User user, DataEntry data) { //Return type is temporary
+	public DataEntry decrypt(User user, DataEntry data) throws IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException { //Return type is temporary
+		try {
+			Key key = keyGen(user);
+			List<String> dataList = new ArrayList<String>();
+			Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			c.init(Cipher.ENCRYPT_MODE, key);
+			for(String entry : data.getFieldDataList()){	
+				BASE64Decoder k = new BASE64Decoder();
+				byte[] decryptedBytes = k.decodeBuffer(entry);
+				byte[] decryptedData = c.doFinal(decryptedBytes);
+				String output = new String(decryptedData);
+				dataList.add(output);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return data;
 	}
