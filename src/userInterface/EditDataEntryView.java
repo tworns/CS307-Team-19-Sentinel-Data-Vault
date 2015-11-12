c223b7f291132dbcdfcb2989dc3cf1525bc6785e
tworns <tworns@purdue.edu>
2015-11-12 11:16:30 -0500

    EditDataEntry disposes instead of killing

--- src/userInterface/EditDataEntryView.java
+++ src/userInterface/EditDataEntryView.java
@@ -62,7 +62,7 @@ public class EditDataEntryView {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		String entryType = currentEntry.getEntryType();
 		if (entryType.equals("SSN")) {
