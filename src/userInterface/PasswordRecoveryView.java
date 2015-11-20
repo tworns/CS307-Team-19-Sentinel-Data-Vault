2519fc5ab215c127a5e9709dc8419373b2712a01
tworns <tworns@purdue.edu>
2015-11-20 13:45:09 -0500

    Removed some swearing and commented code

--- src/userInterface/PasswordRecoveryView.java
+++ src/userInterface/PasswordRecoveryView.java
@@ -107,63 +107,8 @@ public class PasswordRecoveryView {
 		} 
 		}
 		else  {
-			System.out.println("Ya dun fucked up.");
+			System.out.println("Ya dun goofed up.");
 		}
 		
 		}
-		/*JButton btnRecover = new JButton("Recover");
-		btnRecover.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(user.getSecurityAnswer().equals(textField_1)){
-					JOptionPane.showMessageDialog(null, "An email containing your password reset security code has been sent to" + user.getUsername() + ". \n"
-					 		+ "Emails may not be delivered immediately, but if you do not recieve this email in a timely manner, please check your spam filter.", "Change Password", 0);
-					try {
-						SaltGenerator pepper = new SaltGenerator();
-						 securityCode = pepper.generateSalt();
-						 System.out.println(securityCode);
-						 
-						VaultController.Send("sentineldatavault", "SENTINELDATA", user.getUsername(), 
-								"Security Warning", "Dear user,\n\n To continue recovering your account password, please enter the code below into the Sentinel Data Vault.\n"
-										+ "If you did not initiate this change, this e-mail can be safely disregarded.\n\n"+
-										"Your security code is: " + securityCode +"\n\n" +
-										"Sincerely,\nSentinel Data Vault Team");
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				}
-				String inputCode = textField_2.getText();
-				if(securityCode != null && securityCode.equals(inputCode) ){ 
-					//PasswordRecoveryChangeView k = new PasswordRecoveryChangeView(); //TODO implement PasswordRecoveryChangeView
-				}
-			}
-		});
-		btnRecover.setBounds(55, 194, 97, 25);
-		frmSentinelDataVault.getContentPane().add(btnRecover);
-		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				frmSentinelDataVault.dispose();
-			}
-		});
-		btnCancel.setBounds(204, 194, 97, 25);
-		frmSentinelDataVault.getContentPane().add(btnCancel);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(52, 141, 116, 22);
-		frmSentinelDataVault.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblSecurityCode = new JLabel("Security Code");
-		lblSecurityCode.setBounds(204, 144, 97, 16);
-		frmSentinelDataVault.getContentPane().add(lblSecurityCode);
-		
-		txtQuestion = new JTextField();
-		txtQuestion.setText(user.getSecurityQuestion());
-		txtQuestion.setBounds(52, 71, 306, 22);
-		frmSentinelDataVault.getContentPane().add(txtQuestion);
-		txtQuestion.setColumns(10);
-		*/
-
 }
