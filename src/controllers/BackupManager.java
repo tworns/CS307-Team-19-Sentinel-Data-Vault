38b4ba8e5db9d410530bbbfabedc78e33d7c8b59
Adam Petty <iamadampetty@gmail.com>
2015-11-23 12:33:10 -0500

    documentation

--- src/controllers/BackupManager.java
+++ src/controllers/BackupManager.java
@@ -7,22 +7,27 @@ import dataManagement.User;
 
 public class BackupManager {
 	
+	/**
+	 * Generates a new backup database file containing ONLY the current user and their associated data entries
+	 * 
+	 * @param user User object to create an account backup database file for
+	 */
 	public void createUserBackupDatabase(User user) {
 		// Retrieve all of user's data entries
-		DatabaseManager oldDBM = new DatabaseManager("vault_database");
-		List<DataEntry> dataEntryList = oldDBM.retrieveUserDataEntries(user.getUsername());
+		DatabaseManager currentDBM = new DatabaseManager("vault_database");
+		List<DataEntry> dataEntryList = currentDBM.retrieveUserDataEntries(user.getUsername());
 		// Parse username from user_email to use as backup database name
 		String username = user.getUsername().substring(0, user.getUsername().indexOf('@'));
 		String backup_database_name = username + "_backup_database";
 		// Create a new backup database to store
 		// Create the users and data_entries tables in new database
-		DatabaseManager newDBM = new DatabaseManager(backup_database_name);
-		newDBM.createUsersTable();
-		newDBM.createDataEntriesTable();
+		DatabaseManager backupDBM = new DatabaseManager(backup_database_name);
+		backupDBM.createUsersTable();
+		backupDBM.createDataEntriesTable();
 		// Fill the new database with the user and their entries from the result set
-		newDBM.addUserToDatabase(user);
+		backupDBM.addUserToDatabase(user);
 		for (DataEntry entry : dataEntryList) {
-			newDBM.addEntryToDatabase(user, entry);
+			backupDBM.addEntryToDatabase(user, entry);
 		}
 		
 	}
