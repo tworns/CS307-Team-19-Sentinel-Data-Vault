3efa1cdcaad207fa54952b390110ffffbf0c893d
tworns <tworns@purdue.edu>
2015-12-01 01:55:35 -0500

    Call me James Cameron, 'cause I'm working on Avatar
    Added radio buttons in preparation for future pictures. I'm looking at
    100 x 100 px (height x width) as a max size for the pictures.

--- src/userInterface/AvatarView.java
+++ src/userInterface/AvatarView.java
@@ -9,8 +9,12 @@ import com.oracle.webservices.internal.api.databinding.Databinding.Builder;
 
 import java.awt.BorderLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class AvatarView {
+public class AvatarView { //ANY NEW AVATAR PHOTO SHOULD BE ~100 X ~75 (height X width).
 
 	private JFrame frame;
 
@@ -42,13 +46,49 @@ public class AvatarView {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 747, 418);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("");
+		rdbtnNewRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//set user picture to string that'll be assigned in a global array corresponding to the position on the gui.
+				// Gui looks like: 
+				//| 1 | 2 | 3 | 
+				//| 4 | 5 | 6 | ...  
+				// can be expanded to fit needs, preferably continuing with rows of 3. 
+				frame.dispose();
+				
+			}
+		});
+		rdbtnNewRadioButton.setBounds(115, 144, 25, 25);
+		frame.getContentPane().add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("");
+		rdbtnNewRadioButton_1.setBounds(327, 144, 25, 25);
+		frame.getContentPane().add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("");
+		rdbtnNewRadioButton_2.setBounds(569, 144, 25, 25);
+		frame.getContentPane().add(rdbtnNewRadioButton_2);
+		
+		JRadioButton radioButton = new JRadioButton("");
+		radioButton.setBounds(115, 319, 25, 25);
+		frame.getContentPane().add(radioButton);
+		
+		JRadioButton radioButton_1 = new JRadioButton("");
+		radioButton_1.setBounds(327, 319, 25, 25);
+		frame.getContentPane().add(radioButton_1);
+		
+		JRadioButton radioButton_2 = new JRadioButton("");
+		radioButton_2.setBounds(569, 319, 25, 25);
+		frame.getContentPane().add(radioButton_2);
+		
 		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(12, 13, 56, 16);
+		lblNewLabel.setIcon(new ImageIcon(AvatarView.class.getResource("/Avatars/Yep.jpg")));
+		lblNewLabel.setBounds(94, 26, 75, 100);
 		frame.getContentPane().add(lblNewLabel);
-		lblNewLabel.setIcon(new ImageIcon(Builder.class.getResource("/Avatars/Yep.jpg")));
 	}
 }
