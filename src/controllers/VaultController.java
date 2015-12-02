4cfbc104e5c153542b075c2c7fbfe95ab688243e
Steven Sun <stevesunmkii@gmail.com>
2015-12-02 18:56:59 -0500

    password change reminder

--- src/controllers/VaultController.java
+++ src/controllers/VaultController.java
@@ -191,8 +191,40 @@ public class VaultController {
 	
 	//*****TODO: When user successfully login, we need to update the last login time******
 	public int loginCheck(String username, String password) throws NoSuchAlgorithmException {
+		
 		DatabaseManager d = new DatabaseManager("vault_database");
 		User user =d.retrieveUserFromDatabase(username);
+		
+		LocalDateTime fromDateTime = user.getLastLogin();
+		LocalDateTime toDateTime = LocalDateTime.now();
+		
+		LocalDateTime tempDateTime = LocalDateTime.from( fromDateTime );
+		
+		long years = tempDateTime.until( toDateTime, ChronoUnit.YEARS);
+		tempDateTime = tempDateTime.plusYears( years );
+		
+		long months = tempDateTime.until( toDateTime, ChronoUnit.MONTHS);
+		tempDateTime = tempDateTime.plusMonths( months );
+		
+		long days = tempDateTime.until( toDateTime, ChronoUnit.DAYS);
+		tempDateTime = tempDateTime.plusDays( days );
+		
+		
+		long hours = tempDateTime.until( toDateTime, ChronoUnit.HOURS);
+		tempDateTime = tempDateTime.plusHours( hours );
+		
+		long minutes = tempDateTime.until( toDateTime, ChronoUnit.MINUTES);
+		tempDateTime = tempDateTime.plusMinutes( minutes );
+
+		long seconds = tempDateTime.until( toDateTime, ChronoUnit.SECONDS);
+		
+		
+		
+		String freq = user.getBackupFrequency();
+		if(seconds>30){
+			JOptionPane.showMessageDialog(null,"It's been a week since the last time you change your password!");
+		}
+		
 		if(user == null){
 			//JOptionPane.showMessageDialog(null,"Incorrect email / password!");
 			return 0;
